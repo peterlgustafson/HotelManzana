3cb07aca1bdddb5e9c8fac2f2af0ac587e928f08
Peter Gustafson <petergustafson@peters-macbook-air.local>
2019-03-23 12:43:47 -0500

    Initial Commit

---
+++ HotelManzana/AddRegistrationTableViewController.swift
@@ -0,0 +1,266 @@
+//
+//  AddRegistrationTableViewController.swift
+//  HotelManzana
+//
+//  Created by Peter Gustafson on 3/23/19.
+//  Copyright © 2019 Peter Gustafson. All rights reserved.
+//
+
+import UIKit
+
+class AddRegistrationTableViewController: UITableViewController {
+    
+    //Outlets for First Name, Last Name & Email Fields
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    //Action for Done Bar Button Tapped
+    
+    @IBAction func doneBarButtonTapped(_ sender: UIBarButtonItem) {
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
+        let numberOfAdults = Int(numberOfAdultsStepper.value)
+        let numberOfChildren = Int(numberOfChildrenStepper.value)
+        let hasWifi = wifiSwitch.isOn
+        
+        print("DONE TAPPED")
+        print("firstName : \(firstName)")
+        print("lastName : \(lastName)")
+        print("email : \(email)")
+        print("checkIn: \(checkInDate)")
+        print("checkOut: \(checkOutDate)")
+        print("numberOfAdults: \(numberOfAdults)")
+        print("numberOfChildren : \(numberOfChildren)")
+        print("wifi: \(hasWifi)")
+    }
+    
+    //Outlets for Check In & Check Out Date Labels
+    
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    //Action to Update Date Value After User Selection
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        updateViews()
+    }
+    
+    //Function to Update View After Date Picker Selected
+    func updateViews() {
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
+        
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        checkInDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        
+        checkOutDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        
+    }
+    
+    //Adjusting Cell Heights
+    
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    
+    var isCheckInDatePickerShown : Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerShown
+        }
+    }
+    
+    var isCheckOutDatePickerShown: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerShown
+        }
+    }
+    
+    //Function to Not Open Date Picker Upon App Loading
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row):
+            if isCheckInDatePickerShown {
+                return 216.0
+            } else {
+                return 0.0
+            }
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row):
+            if isCheckOutDatePickerShown {
+                return 216.0
+            } else {
+                return 0.0
+            }
+        default:
+            return 44.0
+        }
+    }
+    
+    //Function to Pop Out the Date Picker and then Collapse It Once User Selects Date
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row - 1):
+            
+            if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+            } else if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+                isCheckInDatePickerShown = true
+            } else {
+                isCheckInDatePickerShown = true
+            }
+            
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row - 1):
+            if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+            } else if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+                isCheckOutDatePickerShown = true
+            } else {
+                isCheckOutDatePickerShown = true
+            }
+            
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        default:
+            break
+            
+        }
+    }
+    
+    //Outlets for Adult & Children Value Labels & Steppers
+    
+    @IBOutlet weak var numberOfAdultsLabel: UILabel!
+    
+    @IBOutlet weak var numberOfAdultsStepper: UIStepper!
+    
+    @IBOutlet weak var numberOfChildrenLabel: UILabel!
+    
+    @IBOutlet weak var numberOfChildrenStepper: UIStepper!
+    
+    
+    //Function to Update Adult & Children Views
+    
+    func updateNumberOfGuests() {
+        numberOfAdultsLabel.text = "\(Int(numberOfAdultsStepper.value))"
+        numberOfChildrenLabel.text = "\(Int(numberOfChildrenStepper.value))"
+    }
+    
+    //Action to Update Stepper Value for Adult & Children Value
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        updateNumberOfGuests()
+    }
+    
+    //Update Wi-Fi View
+    
+    @IBOutlet weak var wifiSwitch: UISwitch!
+    
+    @IBAction func wifiSwitchChanged(_ sender: UISwitch) {
+        //add later
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        
+        updateNumberOfGuests()
+        
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+//
+//    // MARK: - Table view data source
+//
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+//
+//    /*
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+//
+//        // Configure the cell...
+//
+//        return cell
+//    }
+//    */
+//
+//    /*
+//    // Override to support conditional editing of the table view.
+//    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        // Return false if you do not want the specified item to be editable.
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // Override to support editing the table view.
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            // Delete the row from the data source
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        } else if editingStyle == .insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//        }
+//    }
+//    */
+//
+//    /*
+//    // Override to support rearranging the table view.
+//    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+//
+//    }
+//    */
+//
+//    /*
+//    // Override to support conditional rearranging of the table view.
+//    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+//        // Return false if you do not want the item to be re-orderable.
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+
+}
